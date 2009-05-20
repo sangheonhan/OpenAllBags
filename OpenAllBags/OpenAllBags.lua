7af4a4d0f0768dddd472bb7bc41db8f6094f6027
Sven Kirmess <sven.kirmess@kzone.ch>
2009-05-20 21:28:58 +0000

    OpenAllBags-3
    git-svn-id: https://open-all-bags.googlecode.com/svn/tags/OpenAllBags-3@4 cfe7c634-4578-11de-aa63-c184e82abc01

---
+++ OpenAllBags/OpenAllBags.lua
@@ -0,0 +1,74 @@
+
+local OpenAllBags_Version = 3
+local OpenAllBags_loaded = false
+
+
+
+function OpenAllBags_OpenAllBankBags()
+
+	local i
+
+        for i = NUM_BAG_SLOTS, (NUM_BAG_SLOTS + NUM_BANKBAGSLOTS) do
+                OpenBag(i)
+        end
+end
+
+
+
+function OpenAllBags_OnEvent(event)
+
+	 if ( event == "PLAYER_ENTERING_WORLD" ) then
+		this:RegisterEvent("AUCTION_HOUSE_SHOW")
+		this:RegisterEvent("AUCTION_HOUSE_CLOSED")
+		this:RegisterEvent("BANKFRAME_OPENED")
+		this:RegisterEvent("BANKFRAME_CLOSED")
+		this:RegisterEvent("GUILDBANKFRAME_OPENED")
+		this:RegisterEvent("GUILDBANKFRAME_CLOSED")
+		this:RegisterEvent("MAIL_SHOW")
+		this:RegisterEvent("MAIL_CLOSED")
+		this:RegisterEvent("MERCHANT_SHOW")
+		this:RegisterEvent("MERCHANT_CLOSED")
+		this:RegisterEvent("TRADE_CLOSED")
+		this:RegisterEvent("TRADE_SHOW")
+		
+		if (not OpenAllBags_loaded) then
+			DEFAULT_CHAT_FRAME:AddMessage(string.format("OpenAllBags %i loaded.", OpenAllBags_Version))
+			OpenAllBags_loaded = true
+		end
+	elseif ( event == "PLAYER_LEAVING_WORLD" ) then
+		this:UnregisterEvent("AUCTION_HOUSE_SHOW")
+		this:UnregisterEvent("AUCTION_HOUSE_CLOSED")
+		this:UnregisterEvent("BANKFRAME_OPENED")
+		this:UnregisterEvent("BANKFRAME_CLOSED")
+		this:UnregisterEvent("GUILDBANKFRAME_OPENED")
+		this:UnregisterEvent("GUILDBANKFRAME_CLOSED")
+		this:UnregisterEvent("MAIL_SHOW")
+		this:UnregisterEvent("MAIL_CLOSED")
+		this:UnregisterEvent("MERCHANT_SHOW")
+		this:UnregisterEvent("MERCHANT_CLOSED")
+		this:UnregisterEvent("TRADE_CLOSED")
+		this:UnregisterEvent("TRADE_SHOW")
+	elseif (
+		event == "AUCTION_HOUSE_SHOW" or
+		event == "BANKFRAME_OPENED" or
+		event == "GUILDBANKFRAME_OPENED" or
+		event == "MAIL_SHOW" or
+		event == "MERCHANT_SHOW" or
+		event == "TRADE_SHOW"
+	) then
+		OpenAllBags(true)
+	elseif (
+		event == "AUCTION_HOUSE_CLOSED" or
+		event == "BANKFRAME_CLOSED" or
+		event == "GUILDBANKFRAME_CLOSED" or
+		event == "MAIL_CLOSED" or
+		event == "MERCHANT_CLOSED" or
+		event == "TRADE_CLOSE"
+	) then
+		local i
+		for i = 0, NUM_BAG_SLOTS do
+			CloseBag(i)
+		end
+        end
+end
+
---
+++ OpenAllBags/readme.txt
@@ -0,0 +1,19 @@
+
+Whenever you interact with a mailbox, the bank, the auction house,
+a merchant or trade with another player, this add-on opens all
+your bags.
+
+This was inspired by the existing OpenBags add-on which did not
+work together with "Soul Pouch".
+
+*** Changelog
+
+Version 3
+Updated TOC for WoW 3.0.2
+
+Version 2
+Added support for guild bank (reported by Greymalkin)
+Removed unused code (reported by Greymalkin)
+
+Version 1
+Initial release

be463e0563be398cdd69ae815df687407894812f
Sven Kirmess <sven.kirmess@kzone.ch>
2010-07-05 20:29:55 +0000

    version 6
    git-svn-id: https://open-all-bags.googlecode.com/svn/tags/OpenAllBags-6@20 cfe7c634-4578-11de-aa63-c184e82abc01

---
+++ OpenAllBags/OpenAllBags.lua
@@ -0,0 +1,69 @@
+
+-- Copyright (c) 2009, Sven Kirmess
+
+local Version = 6
+local Loaded = false
+
+local function EventHandler(self, event, ...)
+
+	 if ( event == "PLAYER_ENTERING_WORLD" ) then
+		self:RegisterEvent("AUCTION_HOUSE_SHOW")
+		self:RegisterEvent("AUCTION_HOUSE_CLOSED")
+		self:RegisterEvent("BANKFRAME_OPENED")
+		self:RegisterEvent("BANKFRAME_CLOSED")
+		self:RegisterEvent("GUILDBANKFRAME_OPENED")
+		self:RegisterEvent("GUILDBANKFRAME_CLOSED")
+		self:RegisterEvent("MAIL_SHOW")
+		self:RegisterEvent("MAIL_CLOSED")
+		self:RegisterEvent("MERCHANT_SHOW")
+		self:RegisterEvent("MERCHANT_CLOSED")
+		self:RegisterEvent("TRADE_CLOSED")
+		self:RegisterEvent("TRADE_SHOW")
+
+		if ( not Loaded ) then
+			DEFAULT_CHAT_FRAME:AddMessage(string.format("OpenAllBags %i loaded.", Version))
+			Loaded = true
+		end
+	elseif ( event == "PLAYER_LEAVING_WORLD" ) then
+		self:UnregisterEvent("AUCTION_HOUSE_SHOW")
+		self:UnregisterEvent("AUCTION_HOUSE_CLOSED")
+		self:UnregisterEvent("BANKFRAME_OPENED")
+		self:UnregisterEvent("BANKFRAME_CLOSED")
+		self:UnregisterEvent("GUILDBANKFRAME_OPENED")
+		self:UnregisterEvent("GUILDBANKFRAME_CLOSED")
+		self:UnregisterEvent("MAIL_SHOW")
+		self:UnregisterEvent("MAIL_CLOSED")
+		self:UnregisterEvent("MERCHANT_SHOW")
+		self:UnregisterEvent("MERCHANT_CLOSED")
+		self:UnregisterEvent("TRADE_CLOSED")
+		self:UnregisterEvent("TRADE_SHOW")
+	elseif (
+		event == "AUCTION_HOUSE_SHOW" or
+		event == "BANKFRAME_OPENED" or
+		event == "GUILDBANKFRAME_OPENED" or
+		event == "MAIL_SHOW" or
+		event == "MERCHANT_SHOW" or
+		event == "TRADE_SHOW"
+	) then
+		OpenAllBags(true)
+	elseif (
+		event == "AUCTION_HOUSE_CLOSED" or
+		event == "BANKFRAME_CLOSED" or
+		event == "GUILDBANKFRAME_CLOSED" or
+		event == "MAIL_CLOSED" or
+		event == "MERCHANT_CLOSED" or
+		event == "TRADE_CLOSE"
+	) then
+		local i
+		for i = 0, NUM_BAG_SLOTS do
+			CloseBag(i)
+		end
+        end
+end
+
+-- main
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("PLAYER_LEAVING_WORLD")
+frame:SetScript("OnEvent", EventHandler)
+
